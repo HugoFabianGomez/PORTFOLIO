165ece58fad3201490d4b64b3185d7300e17113a
HugoFabianGomez <hfg.hugo@hotmail.com>
2021-05-22 10:39:46 -0300

    Initial commit

---
+++ funciones.js
@@ -0,0 +1,7 @@
+function sumar()
+{
+    var numero1 = parseInt(document.getElementById("txtNumero1").value);
+    var numero2 = parseInt(document.getElementById("txtNumero2").value);
+    alert("La suma es "+ (numero1 + numero2));
+
+}
